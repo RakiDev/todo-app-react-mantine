d8baf80f4d3db172d04448f1049de0f49af9f1a3
Raki <killer.vp.mic@gmail.com>
2023-01-29 22:15:30 -0300

    pwa manifest/index.html requirements

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,41 @@
-import { VitePWA } from 'vite-plugin-pwa'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import virtual from 'vite-plugin-virtual';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    virtual(),
     VitePWA({ 
       registerType: 'autoUpdate',
-      injectRegister: 'inline'
+      injectRegister: 'inline',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'TODO Simple',
+        short_name: 'TDS',
+        description: 'Simple TODO app to accomplish your tasks!',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
    })
   ]
 });
