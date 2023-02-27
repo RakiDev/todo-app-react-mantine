1b35f9129926a18a487ef7ebfa73eed1aa6af92f
Alejandro Pedemonte <57377821+ademondev@users.noreply.github.com>
2023-02-27 20:28:25 -0300

    Update Info.tsx

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -9,12 +9,12 @@ const Info: FC = () => {
         <>
         <Paper withBorder style={{ borderRadius: '4px' }}>
             <Text ta='center' weight='bolder' variant="gradient">
-                This app was made with ❤ by RakiDev
+                This app was made with ❤ by ademondev
             </Text>
         </Paper>
         <Divider my="md" label="Contact" labelPosition="center"  style={{ fontWeight: 'bolder'}}/>
         <Flex align='center' justify='center'>
-            <Button component="a" target='_blank' href="https://github.com/RakiDev" leftIcon={<AiFillGithub/>} style={{ backgroundColor: '#0d1117', width: '40%' }}>
+            <Button component="a" target='_blank' href="https://github.com/ademondev" leftIcon={<AiFillGithub/>} style={{ backgroundColor: '#0d1117', width: '40%' }}>
                 GitHub
             </Button>
             <Button component="a" target='_blank' href="https://discord.com/users/218062175636291586" leftIcon={<FaDiscord />} style={{ backgroundColor: '#5764f2', width: '40%' }}>
